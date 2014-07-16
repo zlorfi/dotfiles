59eb0ba477735910f640bff88f728d8a530681dc
michi <zlorfi@gmail.com>
2014-07-16 13:15:40 +0200

    make file extended

--- make_my_dotfiles.sh
+++ make_my_dotfiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# some variables
+_zsh_file="/usr/local/bin/zsh"
+
 # Generate symlinks for files
 for i in gemrc gvimrc irbrc jumprc NERDTreeBookmarks screenrc vimrc zshrc tmux.conf
 do
@@ -7,7 +10,7 @@ do
   then
     ln -s $HOME/dotfiles/$i $HOME/.$i
   else
-    echo Symlink $HOME/.$i already exists
+    echo "Symlink $HOME/.$i already exists"
   fi
 done
 
@@ -17,7 +20,7 @@ do
   then
     ln -s $HOME/dotfiles/$j $HOME/.$j
   else
-    echo Symlink $HOME/.$j already exists
+    echo "Symlink $HOME/.$j already exists"
   fi
 done
 
@@ -25,5 +28,31 @@ if [ ! -d $HOME/.vim/tmp ]
 then
   mkdir $HOME/.vim/tmp
 else
-  echo tmp folder $HOME/.vim/tmp already exists
+  echo "tmp folder $HOME/.vim/tmp already exists"
+fi
+
+# install Homebrew
+if [ ! -f `which brew` ]
+then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+  echo "Homebrew already installed"
+fi
+
+if [ -f `which brew` ]
+then
+  `which brew` install erlang imagemagick mongodb postgresql rbenv redis ruby-build tmux unrar yasm youtube-dl zsh
+else
+  echo "Homebrew not installed"
+fi
+
+if [ ! -f $_zsh_file ]
+then
+  echo
+  echo "Changing shell to ZSH, need sudo password"
+  echo
+  echo $_zsh_file | sudo tee -a /etc/shells;
+  chsh -s $_zsh_file;
+else
+  echo "ZSH already installed"
 fi
