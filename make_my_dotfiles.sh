29f7a8dbb342b03588511a2c22f3c18e33e6c666
Michael Skrynski <zlorfi@gmail.com>
2014-09-12 13:45:19 +0200

    wiki update

--- make_my_dotfiles.sh
+++ make_my_dotfiles.sh
@@ -3,7 +3,7 @@
 # some variables
 _zsh_file="/usr/local/bin/zsh"
 _homebrew=`which brew`
-_to_install="csshx erlang imagemagick mongodb postgresql rbenv redis ruby-build tmux unrar yasm youtube-dl zsh peco elixir"
+_to_install="csshx erlang imagemagick mongodb postgresql rbenv redis ruby-build tmux unrar yasm youtube-dl zsh peco elixir git gcc46"
 
 # Generate symlinks for files
 for i in gemrc gvimrc irbrc jumprc NERDTreeBookmarks screenrc vimrc zshrc tmux.conf
