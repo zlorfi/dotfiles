822487a0d2f89e3bf0ef458e8d0f8d21a1845127
Michael Skrynski <zlorfi@gmail.com>
2017-06-28 14:10:23 +0200

    adding platform_sh alias

--- zsh/lib/platform_sh.zsh
+++ zsh/lib/platform_sh.zsh
@@ -1,3 +1,5 @@
 # Automatically added by Platform.sh CLI installer
 export PATH="/Users/michi/.platformsh/bin:$PATH"
 . '/Users/michi/.platformsh/shell-config.rc' 2>/dev/null
+
+alias psh='platform'
