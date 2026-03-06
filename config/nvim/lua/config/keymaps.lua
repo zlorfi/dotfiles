9975a2f2ae081b25b018df9e1c39ebf6ab5cbca7
Michael Skrynski <zlorfi@gmail.com>
2026-03-06 16:19:48 +0100

    adding custom LazyVim keybindings

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Window navigation with <leader>w + arrow keys (replaces <leader>wh/j/k/l)
+vim.keymap.set("n", "<leader>w<Left>", "<C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("n", "<leader>w<Down>", "<C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<leader>w<Up>", "<C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<leader>w<Right>", "<C-w>l", { desc = "Go to Right Window" })
--- make_my_dotfiles.sh
+++ make_my_dotfiles.sh
@@ -3,55 +3,48 @@
 # some variables
 _shell="/opt/homebrew/bin/zsh"
 # _shell="/opt/homebrew/bin/fish"
-_homebrew=`which brew`
-_to_install="btop gh imagemagick rbenv ruby-build tmux iperf3 zsh oh-my-posh gs tectonic tree-sitter\
+_homebrew=$(which brew)
+_to_install="btop gh imagemagick rbenv ruby-build tmux iperf3 zsh oh-my-posh gs tectonic tree-sitter ripgrep\
              git git-flow nvim azure-cli openssl kubectl typst thonny k9s uv stern nvm lazygit difftastic"
 
 # Generate symlinks for files
-for i in gitconfig gitmessage gitignore tmux.conf prettierrc zsh_secrets
-do
-  if [ ! -f $HOME/.$i ]
-  then
+for i in gitconfig gitmessage gitignore tmux.conf prettierrc zsh_secrets; do
+  if [ ! -f $HOME/.$i ]; then
     ln -s $HOME/dotfiles/$i $HOME/.$i
   else
     echo "Symlink $HOME/.$i already exists"
   fi
 done
 
-if [ ! -d $HOME/.config ]
-then
-    mkdir -p $HOME/.config
-    ln -s $HOME/dotfiles/config $HOME/.config
+if [ ! -d $HOME/.config ]; then
+  mkdir -p $HOME/.config
+  ln -s $HOME/dotfiles/config $HOME/.config
 else
-    echo "Symlink $HOME/.config already exists"
+  echo "Symlink $HOME/.config already exists"
 fi
 
 # install Homebrew
-if [ ! -f $_homebrew ]
-then
+if [ ! -f $_homebrew ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   echo "Homebrew already installed"
 fi
 
 # install some stuff
-if [ -f $_homebrew ]
-then
+if [ -f $_homebrew ]; then
   $_homebrew install $_to_install
 else
   echo "Homebrew not installed"
 fi
 
 # check if $SHELL is already set to zsh
-if [ "$(echo $SHELL)" != "$_shell" ]
-then
-  if [ -f $_shell ]
-  then
+if [ "$(echo $SHELL)" != "$_shell" ]; then
+  if [ -f $_shell ]; then
     echo
     echo "Changing shell to zsh, need sudo password"
     echo
-    echo $_shell | sudo tee -a /etc/shells;
-    chsh -s $_shell;
+    echo $_shell | sudo tee -a /etc/shells
+    chsh -s $_shell
   else
     echo
     echo "Install zsh via Homebrew first!"
