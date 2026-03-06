5ef3486066f4e0b972dba6745cf4cfdc8f7d7255
Michael Skrynski <zlorfi@gmail.com>
2026-03-06 18:04:43 +0100

    customizing mini.files keybinding

---
+++ config/nvim/lua/plugins/mini-files-config.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-mini/mini.files",
+  opts = {
+    mappings = {
+      close = "q",
+      go_in = "<Right>",
+      go_in_plus = "L",
+      go_out = "<Left>",
+      go_out_plus = "H",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+    },
+    windows = {
+      width_preview = 80,
+    },
+  },
+}
