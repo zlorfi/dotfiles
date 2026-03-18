7c16e5201010ea216b62bf1a4dfe7fc82ca7583e
Michael Skrynski <zlorfi@gmail.com>
2026-03-18 06:33:16 +0100

    new agents added

--- make_my_dotfiles.sh
+++ make_my_dotfiles.sh
@@ -4,7 +4,7 @@
 _shell="/opt/homebrew/bin/zsh"
 # _shell="/opt/homebrew/bin/fish"
 _homebrew=$(which brew)
-_to_install="azure-cli btop difftastic gh git git-flow gs imagemagick iperf3 k9s kubectl lazygit mactop nvim nvm oh-my-posh openssl rbenv ripgrep ruby-build stern tectonic thonny tmux tpm tree-sitter typst uv zsh"
+_to_install="azure-cli btop difftastic gh git git-flow gs imagemagick iperf3 k9s kubectl lazygit mactop mole nvim nvm oh-my-posh openssl rbenv ripgrep ruby-build stern tectonic thonny tmux tpm tree-sitter typst uv zsh"
 
 # Generate symlinks for files
 for i in gitconfig gitignore gitmessage tmux.conf zshrc zsh_secrets; do
